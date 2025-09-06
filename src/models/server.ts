74e20d5ffc3ac9451092e9cd2dbfba4d2cd4f599
Rafan Syed <rafansyed@Rafans-MacBook-Pro-2.local>
2025-09-05 22:06:38 -0400

    changed to use database url

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,18 +1,12 @@
-// server.ts
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const CORE_DB = new Sequelize(
-  process.env.DATABASE_NAME || "kufah_attendance",
-  process.env.DATABASE_USER || "kufah_admin",
-  process.env.DATABASE_PASSWORD || "password",
-  {
-    host: process.env.DATABASE_HOST || "localhost",
-    port: Number(process.env.DATABASE_PORT) || 5432,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const databaseUrl = process.env.DATABASE_URL; // put the full Render URL in env
+
+const CORE_DB = new Sequelize(databaseUrl || "postgres://kufah_admin:password@localhost:5432/kufah_attendance", {
+  dialect: "postgres",
+  logging: false,
+});
 
 export default CORE_DB;
