04d7c7e525263e4f7e797ef2c33b1971ce65ddb6
Rafan Syed <rafansyed@Rafans-MacBook-Pro-2.local>
2025-09-09 00:04:11 -0400

    added routes so that you can add multiple students to one class

--- src/models/studentClasses/types.ts
+++ src/models/studentClasses/types.ts
@@ -2,3 +2,8 @@ export interface StudentClassRequest {
   classId: number;
   studentId: number;
 }
+
+export interface BulkStudentClassRequest {
+  studentIds: number[]; // array of student IDs
+  classId: number;      // single class ID
+}
