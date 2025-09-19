ea2313866d6650e8f94e94acd0fb92722ae6d824
Rafan Syed <rafansyed@Rafans-MacBook-Pro-2.local>
2025-09-19 12:26:42 -0400

    test email subject change will change back later

--- src/utils/email/sendAttendanceEmail.ts
+++ src/utils/email/sendAttendanceEmail.ts
@@ -8,15 +8,23 @@ export const sendAttendanceEmail = async (
   const today = new Date();
   const attendanceLink = `${process.env.FRONTEND_URL}/pages/attendance?token=${token}`;
 
+  // const mailOptions = {
+  //   from: `"Kufah" <${process.env.SMTP_USER}>`,
+  //   to: studentEmail,
+  //   subject: `Mark your attendance for ${today.toLocaleDateString()}`,
+  //   html: `<p>As-salamu alaykum ${studentName},</p>
+  //          <p>Please mark your attendance for your classes for today by clicking the link below:</p>
+  //          <a href="${attendanceLink}">${attendanceLink}</a>
+  //          <p>JazakAllahu Khairan,<br/>Kufah Team</p>`,
+  // };
+  
   const mailOptions = {
     from: `"Kufah" <${process.env.SMTP_USER}>`,
     to: studentEmail,
-    subject: `Mark your attendance for ${today.toLocaleDateString()}`,
+    subject: `This is a test email please ignore`,
     html: `<p>As-salamu alaykum ${studentName},</p>
-           <p>Please mark your attendance for your classes for today by clicking the link below:</p>
-           <a href="${attendanceLink}">${attendanceLink}</a>
+           <p>Sorry for the inconvenience, but this is a test email.</p>
            <p>JazakAllahu Khairan,<br/>Kufah Team</p>`,
   };
-
   await transporter.sendMail(mailOptions);
 };
